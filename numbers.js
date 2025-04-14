1a2952d95f6c76e4351f79308622c6796010800f
Michael Stokes <michael76stokes@hotmail.com>
2025-04-14 20:42:32 +0100

    add numbers file

---
+++ numbers.js
@@ -0,0 +1,28 @@
+//Simple Opeations
+
+// Addition
+50 + 5; //55
+
+//Subtraction
+90 - 1; //89
+
+//Multiplication
+11111 * 7; //77777
+
+//Division
+400 / 25; //16
+
+//Modulo
+27 % 2; //1
+
+//Exponent
+2 ** 3; //8
+
+//NaN Not a Number
+//NaN is a numeric value that represents something that is...Not a Number
+0 / 0; //NaN
+1 + NaN; //NaN
+
+//Infinity
+1 / 0; //Infinity
+-1 / 0; //Infinity
