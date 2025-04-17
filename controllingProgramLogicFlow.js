728f9a2d08e04014ace3d1ee6a358dff6072d843
Michael Stokes <michael76stokes@hotmail.com>
2025-04-17 21:18:03 +0100

    add JavaScript files

---
+++ controllingProgramLogicFlow.js
@@ -0,0 +1,8 @@
+>   //greater than
+<   //lesser than
+>=  //greater than or equal to
+<=  //lesser than or equal to
+==  //equality
+!=  //non equality
+=== //strict equality
+!== //strict non equality
